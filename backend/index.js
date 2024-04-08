aa02de467e11e7c2db11f8b171c7fc7126f0ef9d
ashkam58 <ashkam58@gmail.com>
2024-04-08 13:50:28 +0530

    no pass

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,7 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
-//ashkam58
-//m9iQMNeSFgmEmnsS
-//mongodb+srv://ashkam58:<password>@yoga-master.lcddpho.mongodb.net/
+
 
 //MONGODB
 
